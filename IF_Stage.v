88720af6c3ae06d1e5de30e34af10116349febac
Alejandro Rios <jassoalex97@hotmail.com>
2019-04-21 16:37:58 -0500

    IF ID Register agregado con sus cambios

--- IF_Stage.v
+++ IF_Stage.v
@@ -13,17 +13,17 @@
  ******************************************************************/
 module IF_Stage #(parameter MEMORY_DEPTH = 256)
                  (input clk,
-                 input reset,
-                 input pc_src_M,
-                 input pc_branch_M,
-                 output reg [31:0] instruction_bus,
-                 output reg [31:0] pc_plus_4_IF);
-
+                  input reset,
+                  input pc_src_M,
+                  input pc_branch_M,
+                  output reg [31:0] instruction_bus,
+                  output reg [31:0] pc_plus_4_IF);
+    
     wire [31:0] pc_wire;
     wire [31:0] next_pc_wire;
     wire [31:0] instruction_bus_wire;
     wire [31:0] pc_plus_4_wire;
-
+    
     PC_Register
     ProgramCounter
     (
@@ -32,7 +32,7 @@ module IF_Stage #(parameter MEMORY_DEPTH = 256)
     .NewPC(next_pc_wire),
     .PCValue(pc_wire)
     );
-
+    
     ProgramMemory
     #(
     .MEMORY_DEPTH(MEMORY_DEPTH)
@@ -42,7 +42,7 @@ module IF_Stage #(parameter MEMORY_DEPTH = 256)
     .Address(pc_wire),
     .Instruction(instruction_bus_wire)
     );
-
+    
     Adder32bits
     PC_Adder
     (
@@ -50,7 +50,7 @@ module IF_Stage #(parameter MEMORY_DEPTH = 256)
     .Data1(4),
     .Result(pc_plus_4_wire)
     );
-
+    
     Multiplexer2to1
     PC_Src_MUX
     (
@@ -59,8 +59,8 @@ module IF_Stage #(parameter MEMORY_DEPTH = 256)
     .MUX_Data1(pc_branch_M),
     .MUX_Output(next_pc_wire)
     );
-
+    
     assign instruction_bus = instruction_bus_wire;
-    assign pc_plus_4_IF = pc_plus_4_wire;
-
+    assign pc_plus_4_IF    = pc_plus_4_wire;
+    
 endmodule
